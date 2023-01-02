cd36eb27d7ca302d074756c6bbf3327ef1746036
ercantekoglan <ercantekoglan@gmail.com>
2023-01-02 02:53:01 -0600

    up

--- mydoctor/src/main/java/security/SecurityConfiguration.java
+++
@@ -1,9 +0,0 @@
-package security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-}
