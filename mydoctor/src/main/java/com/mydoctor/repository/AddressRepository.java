a14b70ef70219040a7eacdfe5ed4eb59b724be85
ercantekoglan <ercantekoglan@gmail.com>
2023-01-02 15:34:45 -0600

    up

--- mydoctor/src/main/java/com/mydoctor/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mydoctor.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mydoctor.domain.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
